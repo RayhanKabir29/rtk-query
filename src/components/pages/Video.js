3dbf11cb7ae778d35813f1ff157bc522b4dd9238
RayhanKabir29 <rayhan.kabir29@gmail.com>
2025-05-20 00:58:32 +0600

    get single video from api

--- src/components/pages/Video.js
+++ src/components/pages/Video.js
@@ -1,20 +1,37 @@
+import { useParams } from "react-router-dom";
+import { useGetVideoQuery } from "../../features/api/apiSlice";
 import Description from "../video/Description";
 import Player from "../video/Player";
 import RelatedVideos from "../video/related/RelatedVideos";
 
 export default function Video() {
-    return (
-        <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
-            <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
-                <div className="grid grid-cols-3 gap-2 lg:gap-8">
-                    <div className="col-span-full w-full space-y-8 lg:col-span-2">
-                        <Player />
-                        <Description />
-                    </div>
+  const { videoId } = useParams();
+  const { data, isError, isLoading, error } = useGetVideoQuery(videoId);
 
-                    <RelatedVideos />
-                </div>
-            </div>
-        </section>
+  let content = null;
+  if (isLoading) content = <p>Loading...</p>;
+  if (!isLoading && isError) content = <p>{error.message}</p>;
+  if (!isLoading && !isError && !data.id === 0)
+    content = <p>No videos found</p>;
+  if (!isLoading && !isError && data.id) {
+    content = (
+      <>
+        <Player link={data?.link} title={data?.title} />
+        <Description data={data} />
+      </>
     );
+  }
+  return (
+    <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
+      <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
+        <div className="grid grid-cols-3 gap-2 lg:gap-8">
+          <div className="col-span-full w-full space-y-8 lg:col-span-2">
+            {content}
+          </div>
+
+          <RelatedVideos />
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,10 +1,16 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
 import Video from "./Video";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            
-        </>
-    );
+  const { data, error, isLoading, isError } = useGetVideosQuery();
+
+  let content = null;
+  if (isLoading) content = <p>Loading...</p>;
+  if (!isLoading && isError) content = <p>{error.message}</p>;
+  if (!isLoading && !isError && data.length === 0)
+    content = <p>No videos found</p>;
+  if (!isLoading && !isError && data.length > 0) {
+    content = data.map((video) => <Video key={video.id} video={video} />);
+  }
+  return <>{content}</>;
 }
