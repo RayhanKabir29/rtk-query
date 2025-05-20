26f4493401d6f2e78aa950ff6df1aea58969922f
RayhanKabir29 <rayhan.kabir29@gmail.com>
2025-05-20 22:11:48 +0600

    edit button

--- src/components/video/related/RelatedVideos.js
+++ src/components/video/related/RelatedVideos.js
@@ -1,21 +1,24 @@
 import { useGetRelatedVideoQuery } from "../../../features/api/apiSlice";
 import RelatedVideo from "./RelatedVideo";
 
-export default function RelatedVideos({id,title}) {
-    const { data, isError, isLoading, error } = useGetRelatedVideoQuery({videoId:id,title:title});
-    let content = null;
-    if (isLoading) content = <p>Loading...</p>;
-    if (!isLoading && isError) content = <p>{error.message}</p>;
-    if (!isLoading && !isError && data.length === 0)
-        content = <p>No videos found</p>;
-    if (!isLoading && !isError && data.length > 0) {
-        content = data.map((video) => (
-            <RelatedVideo key={video.id} video={video} />
-        ));
-    }
-    return (
-        <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
-            {content}
-        </div>
-    );
+export default function RelatedVideos({ id, title }) {
+  const { data, isError, isLoading, error } = useGetRelatedVideoQuery({
+    videoId: id,
+    title: title,
+  });
+  let content = null;
+  if (isLoading) content = <p>Loading...</p>;
+  if (!isLoading && isError) content = <p>{error.message}</p>;
+  if (!isLoading && !isError && data.length === 0)
+    content = <p>No videos found</p>;
+  if (!isLoading && !isError && data.length > 0) {
+    content = data.map((video) => (
+      <RelatedVideo key={video.id} video={video} />
+    ));
+  }
+  return (
+    <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
+      {content}
+    </div>
+  );
 }
